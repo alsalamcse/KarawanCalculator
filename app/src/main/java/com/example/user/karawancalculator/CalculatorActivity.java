1812fad6cb1796e5d359e0a042a8bf47fda7a341
user <tasnem.mobarky@gmail.com>
2017-10-04 12:26:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/karawancalculator/CalculatorActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.karawancalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
